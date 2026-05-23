f55ca7355d1d2ca3ce018d0011b70fdddc0a4166
Rushikesh Sapkal <rushikeshsapkal930@gmail.com>
2026-05-23 11:46:23 +0530

    optimize bundle size by code-splitting

--- rushmail/vite.config.ts
+++ rushmail/vite.config.ts
@@ -21,6 +21,23 @@ export default defineConfig(({ mode }) => {
 			port: 4200,
 			allowedHosts: true,
 			hmr: { overlay: false, }
+		},
+		build: {
+			rollupOptions: {
+				output: {
+					manualChunks(id) {
+						if (id.includes("node_modules")) {
+							if (id.includes("recharts") || id.includes("d3")) {
+								return "vendor-charts";
+							}
+							if (id.includes("tiptap") || id.includes("prosemirror")) {
+								return "vendor-editor";
+							}
+							return "vendor";
+						}
+					}
+				}
+			}
 		}
 	};
 });
