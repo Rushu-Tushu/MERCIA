750b784a7b5543dc101744d390605f556d14a2c6
Rushikesh Sapkal <rushikeshsapkal930@gmail.com>
2026-05-24 07:52:06 +0530

    this is final, I'm done now, I want to rest

--- rushmail/api/index.ts
+++ rushmail/api/index.ts
@@ -2,4 +2,9 @@
 import { handle } from "hono/vercel";
 import app from '../src/api/index.js';
 
-export default handle(app);
+export const GET = handle(app);
+export const POST = handle(app);
+export const PUT = handle(app);
+export const DELETE = handle(app);
+export const OPTIONS = handle(app);
+export const PATCH = handle(app);
